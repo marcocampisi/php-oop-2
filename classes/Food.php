af93222e509c6252cebbe919e5ab54ff9b739f37
marcocampisi <marco.campisi@outlook.it>
2023-09-04 16:16:00 +0200

    Add class Food

---
+++ classes/Food.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__.'/Product.php';
+
+class Food extends Product {
+    public $weight;
+    public $animal;
+
+    public function __construct($id, $image, $title, $price, $category, $weight, $animal) {
+        parent::__construct($id, $image, $title, $price, $category);
+        $this->weight = $weight;
+    }
+
+    public function printCard() {
+        parent::printCard();
+        echo "<p>$this->weight g</p>";
+    }
+}
+
+
+?>
