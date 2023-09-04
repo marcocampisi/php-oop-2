5e6150675ef95560bf44ebdd755fbceb8067de65
marcocampisi <marco.campisi@outlook.it>
2023-09-04 16:10:34 +0200

    Add printCard method

--- classes/Product.php
+++ classes/Product.php
@@ -7,7 +7,22 @@ class Product {
     public $price;
     public $category;
 
-    
+    public function __construct($id, $image, $title, $price, $category) {
+        $this->id = $id;
+        $this->image = $image;
+        $this->title = $title;
+        $this->price = $price;
+        $this->category = $category;
+    }
+
+    public function printCard() {
+        echo "<div class='card'>";
+        echo "<img src='$this->image' alt='$this->title'>";
+        echo "<h3>$this->title</h3>";
+        echo "<p>$this->price €</p>";
+        echo "<span>$this->category</span>";
+        echo "</div>";
+    }
 }
 
 ?>
